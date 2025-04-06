74b486dfcfc33071b978a7aee8f2375b1396660c
ACJUGGLER <am5612474@gmail.com>
2025-04-05 19:01:12 -0500

    Prueba de nuevo

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-saludo = "Santiago es hermoso para Diego"
-print(saludo)
+m = 0b111110011 # 0b111110011
+n = m >> 3
+representacion = bin(n)
+print(representacion)
+
+
+
+'''x = 0o1243
+y = 0x121 
+print(m)
+print(x)
+print(y)
+
+u = bin(1996)
+print(u)'''
+
